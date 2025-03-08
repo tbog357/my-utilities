d02d0a40dd051b5177c639840949d9f1e63c6e67
tbog <hahuydung357@gmail.com>
2025-03-08 11:55:56 +0700

    fix: able to use different namespace for each terminal session

--- kubectl-scripts/set_context_v2.sh
+++ kubectl-scripts/set_context_v2.sh
@@ -2,16 +2,21 @@
 # Get context name
 context_name=$(kubectl config get-contexts | fzf --header-lines=1 | awk '{print $2}' -)
 
-# Copy config file
-cp ${HOME}/.kube/config ${HOME}/.kube/config-${context_name}
+if [ -z $context_name ]; then
+    echo "Not thing" > /dev/null
+else
+    kubectl config use-context ${context_name}
+    namespace_name=$(kubectl config view --minify -o jsonpath='{..namespace}')
+    cp ${HOME}/.kube/config ${HOME}/.kube/config-${context_name}-${namespace_name}
 
-# Export file
-export KUBECONFIG=${HOME}/.kube/config-${context_name}
-kubectl config use-context ${context_name}
+    # Export file
+    export KUBECONFIG=${HOME}/.kube/config-${context_name}-${namespace_name}
+    echo "Using config file at ${KUBECONFIG}"
 
-# Export to env_vars
-namespace_name=$(kubectl config view --minify -o jsonpath='{..namespace}')
+    # Export to env_vars
+    namespace_name=$(kubectl config view --minify -o jsonpath='{..namespace}')
 
-# For terminal to recognize context and namespace
-export K8S_CONTEXT=$context_name
-export K8S_NAMESPACE=$namespace_name
+    # For terminal to recognize context and namespace
+    export K8S_CONTEXT=$context_name
+    export K8S_NAMESPACE=$namespace_name
+fi
--- kubectl-scripts/set_namespace_v2.sh
+++ kubectl-scripts/set_namespace_v2.sh
@@ -1,9 +1,13 @@
 #!/bin/bash
 namespace_name=$(kubectl get ns | fzf --header-lines=1 | cut -d" " -f1)
 
-# Set namespace
-kubectl config set-context --current --namespace=${namespace_name}
-
-export KUBECONFIG=${HOME}/.kube/config-${K8S_CONTEXT}
-export K8S_NAMESPACE=$namespace_name
-
+if [ -z $namespace_name  ] || [ -z $K8S_CONTEXT ]; then
+    echo "Please set_context first"
+else
+    cp $KUBECONFIG ${HOME}/.kube/config-${K8S_CONTEXT}-${namespace_name}
+    export K8S_NAMESPACE=$namespace_name
+    export KUBECONFIG=${HOME}/.kube/config-${K8S_CONTEXT}-${K8S_NAMESPACE}
+    kubectl config use-context ${K8S_CONTEXT}
+    kubectl config set-context --current --namespace=${namespace_name}
+    echo "Using config file at ${KUBECONFIG}"
+fi
